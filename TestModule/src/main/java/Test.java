9b97b8ad3fd67f839ed533b1e2e56094132720de
leungjianjun <leungjianjun@gmail.com>
2012-05-20 10:45:26 +0800

    test

---
+++ TestModule/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ljj-lab
+ * Date: 12-5-20
+ * Time: 上午10:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("Test");
+    }
+}
